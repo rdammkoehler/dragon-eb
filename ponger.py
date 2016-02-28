d0e17225f0c50bd3df55949cc122df8c4beac52b
Rich Dammkoehler <rpd@noradltd.com>
2016-02-28 14:44:52 -0600

    do not persist ping or pong

--- ponger.py
+++ ponger.py
@@ -7,11 +7,11 @@ class Ponger(DragonBusClient):
         DragonBusClient.__init__(self)
         self.callback(self.send_pong)
         self.ignores('event_id', 2)
+        self.ignores('event_id', 1)
 
     def send_pong(self, ch, method, properties, json_message):
-        if not self.ignored(json_message):
-            self.rmq_client.send(Pong().to_json())
-            print("{{{Pong}}}")
+        self.rmq_client.send(Pong().to_json())
+        print("{{{Pong}}}")
 
 if __name__ == "__main__":
     Ponger().start()
