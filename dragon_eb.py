b285abea6e3d801c2e9c5f3316f8f4020294f6df
Rich Dammkoehler <rpd@noradltd.com>
2016-02-28 14:57:24 -0600

    pushed a lot of logic up into dragon_eb from ponger

--- dragon_eb.py
+++ dragon_eb.py
@@ -5,14 +5,17 @@ from rabbit_client import RabbitCommandClient
 
 class DragonBusClient:
 
-    def __init__(self):
+    def __init__(self, persist_messages=False):
         self.mongo_client = MongoClient()
         self.rmq_client = RabbitCommandClient()
-        self.rmq_client.recv(callback=self.store_received_message)
+        self.rmq_client.recv(callback=self.on_message)
+        self.callbacks = []
         self.ignore = {}
+        if persist_messages:
+            self.persist_messages()
 
-    def callback(self, callback):
-        self.callback = callback
+    def add_callback(self, callback):
+        self.callbacks.append(callback)
 
     def ignores(self, key, value):
         if self.ignore and key in self.ignore:
@@ -26,16 +29,24 @@ class DragonBusClient:
     def send(self, json_string):
         self.rmq_client.send(json_string)
 
-    def store_received_message(self, ch, method, properties, message):
-        json_message = json.loads(message.decode('utf-8'))
-        if not self.ignored(json_message):
-            self.mongo_client.dragon.events.insert_one(json_message).inserted_id
-        self.callback(ch, method, properties, json_message)
+    def on_message(self, ch, method, properties, message):
+        if not self.ignored(message):
+            for callback in self.callbacks:
+                callback(ch, method, properties, message)
 
-    def ignored(self, json_message):
+    def ignored(self, message):
+        json_message = json.loads(message.decode('utf-8'))
         for header, values in self.ignore.items():
             if header in json_message['header']:
                 for value in values:
                     if json_message['header'][header] == value:
                         return True
         return False
+
+    def persist_messages(self):
+        self.callback(persist_message)
+
+    def persist_message(self, ch, method, properties, message):
+        json_message = json.loads(message.decode('utf-8'))
+        if not self.ignored(json_message):
+            self.mongo_client.dragon.events.insert_one(json_message).inserted_id
--- ponger.py
+++ ponger.py
@@ -1,3 +1,5 @@
+import json
+
 from dragon_eb import DragonBusClient
 from simple_event import Pong
 
@@ -5,11 +7,10 @@ class Ponger(DragonBusClient):
 
     def __init__(self):
         DragonBusClient.__init__(self)
-        self.callback(self.send_pong)
-        self.ignores('event_id', 2)
-        self.ignores('event_id', 1)
+        self.add_callback(self.send_pong)
 
-    def send_pong(self, ch, method, properties, json_message):
+    def send_pong(self, ch, method, properties, message):
+        json_message = json.loads(message.decode('utf-8'))
         self.rmq_client.send(Pong().to_json())
         print("{{{Pong}}}")
 
