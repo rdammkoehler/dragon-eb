21b8bc25feb0fb6e73c3d2ca0fbeff88aba37dad
Rich Dammkoehler <rpd@noradltd.com>
2016-03-01 11:25:02 -0600

    made ponger persist messages by default

--- ponger.py
+++ ponger.py
@@ -6,10 +6,9 @@ from simple_event import Pong
 class Ponger(DragonBusClient):
 
     def __init__(self):
-        DragonBusClient.__init__(self)
+        DragonBusClient.__init__(self, True)
         self.ignores('event_id', 2)
         self.add_callback(self.send_pong)
-        self.persist_messages()
 
     def send_pong(self, ch, method, properties, message):
         json_message = json.loads(message.decode('utf-8'))
