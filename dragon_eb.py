2bbec745d1d6d5f1f6c3a6a1bf8aa91821cfcd63
Rich Dammkoehler <rpd@noradltd.com>
2016-03-01 13:12:39 -0600

    some better filtering, but not working in a topic as expected

--- dragon_eb.py
+++ dragon_eb.py
@@ -1,30 +1,20 @@
 import json
+import os
 
+from filter import Filter
 from rabbit_client import RabbitCommandClient
+
 
 class DragonBusClient:
 
-    def __init__(self):
+    def __init__(self, message_filter=Filter()):
+        self.message_filter = message_filter
         self.rmq_client = RabbitCommandClient()
         self.rmq_client.recv(callback=self.on_message)
         self.callbacks = []
-        self.listens_for = {}
-        self.ignore = {}
 
     def add_callback(self, callback):
         self.callbacks.append(callback)
-
-    def listen_for(self, key, value):
-        if self.listens_for and key in self.listens_for:
-            self.listens_for[key].append(value)
-        else:
-            self.listens_for[key] = [value]
-
-    def ignores(self, key, value):
-        if self.ignore and key in self.ignore:
-            self.ignore[key].append(value)
-        else:
-            self.ignore[key] = [value]
 
     def start(self):
         self.rmq_client.start()
@@ -34,25 +24,8 @@ class DragonBusClient:
 
     def on_message(self, ch, method, properties, message):
         json_message = json.loads(message.decode('utf-8'))
-        if not self.ignored(json_message) and self.accepted(json_message):
+        if self.message_filter.accept(json_message):
             for callback in self.callbacks:
                 callback(ch, method, properties, json_message)
-
-    def accepted(self, json_message):
-        return self.__contains(self.listens_for, json_message)
-
-    def ignored(self, json_message):
-        return self.__contains(self.ignore, json_message)
-
-    def __contains(self, kvp, json_message):
-        rval = False
-        if type(json_message) is dict:
-            for key, values in kvp.items():
-                if key in json_message:
-                    for value in values:
-                        if json_message[key] == value:
-                            rval = True
-                else:
-                    for json_key in json_message:
-                        rval = rval or self.__contains(kvp, json_message[json_key])
-        return rval        
+        else:
+            print("(%d) not accepted %s" % (os.getpid(), json_message))
--- ponger.py
+++ ponger.py
@@ -1,13 +1,13 @@
 import json
 
 from dragon_eb import DragonBusClient
+from event_id_filter import EventIdFilter
 from simple_event import Pong
 
 class Ponger(DragonBusClient):
 
     def __init__(self):
-        DragonBusClient.__init__(self)
-        self.listen_for('event_id', 1)
+        DragonBusClient.__init__(self, EventIdFilter(1))
         self.add_callback(self.send_pong)
 
     def send_pong(self, ch, method, properties, json_message):
