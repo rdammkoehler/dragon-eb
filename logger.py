176d4cda0342b3181abf2c4b658c4eb5321640a0
Rich Dammkoehler <rpd@noradltd.com>
2016-03-08 13:51:00 -0600

    added SysLogger and tweaked where logged messages get put in the db

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import json
 
 from dragon_eb import DragonBusClient
-from event_id_filter import EventIdRangeExclusionFilter
+from event_id_filter import EventIdRangeFilter, EventIdRangeExclusionFilter
 
 from pymongo import MongoClient
 
@@ -13,7 +13,17 @@ class Logger(DragonBusClient):
         self.add_callback(self.__persist_message)
 
     def __persist_message(self, ch, method, properties, json_message):
-        self.mongo_client.dragon.events.insert_one(json_message)
+        self.mongo_client.dragon.log.insert_one(json_message)
+
+class SysLogger(DragonBusClient):
+    def __init__(self):
+        DragonBusClient.__init__(self, EventIdRangeFilter(0, 99))
+        self.mongo_client = MongoClient()
+        self.add_callback(self.__persist_message)
+
+    def __persist_message(self, ch, method, properties, json_message):
+        self.mongo_client.dragon.sys.insert_one(json_message)
 
 if __name__ == "__main__":
+    SysLogger().start()
     Logger().start().join()
