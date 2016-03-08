6742de29a147b6ab0b259ef96c727afd5d608642
Rich Dammkoehler <rpd@noradltd.com>
2016-03-08 12:10:14 -0600

    simplified resource retriever

--- resource_retriever.py
+++ resource_retriever.py
@@ -1,5 +1,4 @@
 import json
-# import threading
 import time
 import urllib.request
 
@@ -10,24 +9,16 @@ from simple_event import ResourceReady
 class ResourceRetriever(DragonBusClient):
 
     def __init__(self, resource_receiver):
-        self.resource_receiver = resource_receiver
         DragonBusClient.__init__(self, EventIdFilter(2000))
-        self.add_callback(self.__retrieve_resource)
-        # tt = threading.Thread(target=self.start())
-        # tt.setDaemon(True)
-        # tt.start()
-
-    def __retrieve_resource(self, ch, method, properties, json_message):
-        print(urllib.request.urlopen(json_message['body']['resource_url']))
-        with urllib.request.urlopen(json_message['body']['resource_url']) as resource:
-            self.resource_receiver(resource)
+        self.add_callback(resource_receiver)
 
 class ResourceCat:
 
-    def cat(self, resource):
-        print('cat')
-        print(resource.read())
-
+    def cat(self, ch, method, properties, json_message):
+        print(urllib.request.urlopen(json_message['body']['resource_url']))
+        with urllib.request.urlopen(json_message['body']['resource_url']) as resource:
+            print('cat')
+            print(resource.read())
 
 if __name__ == "__main__":
     ResourceRetriever(ResourceCat().cat).start()
